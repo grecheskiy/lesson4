81ca244715761ac422740d8ba5795ba891644e7e
grecheskiy <grecheskiy@rambler.ru>
2023-01-29 13:30:59 +0300

    add homework_4_2

---
+++ homework_4_2/Program.cs
@@ -0,0 +1,27 @@
+﻿//Задайте одномерный массив, заполненный случайными числами. 
+//Найдите сумму элементов, стоящих на нечётных позициях.
+//[3, 7, 23, 12] -> 19
+//[-4, -6, 89, 6] -> 0
+void FillMass(int[] arr)
+{for (int i=0; i<arr.Length; i++)
+{int rnd = new Random().Next(-99,99);
+arr[i] = rnd;}}
+
+void PrintMass(int[] arr)
+{foreach (int item in arr)
+Console.Write($"{item} ");}
+
+int sum = 0;
+void OddMass(int[] arr)
+{for (int j=0; j<arr.Length; j++) 
+{if (j % 2 == 1) sum = sum + arr[j];
+}Console.Write($" -> {sum}");
+Console.WriteLine();    
+}
+
+Console.Write("Enter array size: ");
+int k = int.Parse(Console.ReadLine()?? "");
+int[] arr = new int[k];
+FillMass(arr);
+PrintMass(arr);
+OddMass(arr);
